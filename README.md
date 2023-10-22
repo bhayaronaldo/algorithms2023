d1e1ae40d51c41ee3778ad7543c8963011be9206
Aditya Gupta <adit7179@gmail.com>
2023-10-22 16:45:15 +0530

    aditya created a new Python repository

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # algorithms2023
+
 Repository to implement different algorithms
---
+++ src/aditya_algorithms/test.py
@@ -0,0 +1,9 @@
+
+def printHello(name: str):
+    print(f"Hello {name}")
+
+if __name__ == '__main__':
+    printHello("Aditya")
+
+# printHello("Aditya")
+
